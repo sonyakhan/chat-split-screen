2f39a00347106f7a945b42c061c667223045a131
Sonya Khan <sonya.khan.cs@gmail.com>
2017-07-07 13:22:19 -0400

    changed port for heroku setup

--- server.js
+++ server.js
@@ -44,6 +44,6 @@ io.on('connection', function(socket){
 
 });
 
-server.listen(3000, function() {
+server.listen(process.env.PORT || 3000, function() {
     console.log('Magic happens on port 3000');
 });
